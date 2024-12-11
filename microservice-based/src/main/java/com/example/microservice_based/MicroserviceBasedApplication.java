60d6d67a24a4a54540246fd3721742097583cdcf
boyquynhluu <vanductai.dhv@gmail.com>
2024-12-11 21:17:59 +0700

    Your commit message

---
+++ microservice-based/src/main/java/com/example/microservice_based/MicroserviceBasedApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservice_based;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceBasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceBasedApplication.class, args);
+	}
+
+}
---
+++ microservice-based/src/test/java/com/example/microservice_based/MicroserviceBasedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservice_based;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceBasedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
